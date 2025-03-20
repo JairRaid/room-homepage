3ba976703a415a28f6c596c080ddb57a8832c5ac
JairRaid <rakotoacer@gmail.com>
2025-03-21 00:54:06 +0300

    update layout with white space in main element

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/room-homepage",
+  base: "/room-homepage", //room-homepage
 });
